61247aee72698611018dd3f3cf3866af3b901957
kenpsrossi <kenpsrossi@gmail.com>
2025-03-24 10:57:01 -0300

    teste exercicio extra

--- ValidadorCPF.java
+++ ValidadorCPF.java
@@ -1,10 +1,61 @@
+
 package aula_condicional;
 
+import java.util.Scanner;
+
 public class ValidadorCPF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Olá, vamos validar seu CPF!");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.println("Digite o CPF (apenas números): ");
+        String cpf = sc.next();
 
-	}
+        if (isCPFValido(cpf)) {
+            System.out.println("CPF válido.");
+        } else {
+            System.out.println("CPF inválido.");
+        }
 
+        sc.close();
+    }
+
+    public static boolean isCPFValido(String cpf) {
+        // Remover caracteres não numéricos
+        cpf = cpf.replaceAll("\\D", "");
+
+        // Verificar se o CPF tem 11 dígitos
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        // Verificar se todos os dígitos são iguais
+        if (cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+
+        // Calcular o primeiro dígito verificador
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += (cpf.charAt(i) - '0') * (10 - i);
+        }
+        int primeiroDigitoVerificador = 11 - (soma % 11);
+        if (primeiroDigitoVerificador >= 10) {
+            primeiroDigitoVerificador = 0;
+        }
+
+        // Calcular o segundo dígito verificador
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += (cpf.charAt(i) - '0') * (11 - i);
+        }
+        int segundoDigitoVerificador = 11 - (soma % 11);
+        if (segundoDigitoVerificador >= 10) {
+            segundoDigitoVerificador = 0;
+        }
+
+        // Verificar se os dígitos verificadores são válidos
+        return (cpf.charAt(9) - '0' == primeiroDigitoVerificador) && (cpf.charAt(10) - '0' == segundoDigitoVerificador);
+    }
 }
